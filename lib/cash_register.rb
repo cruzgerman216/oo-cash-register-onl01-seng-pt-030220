fc2968feafa5a742c6daeda49e2490f7646ac10f
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 19:15:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,19 +7,19 @@ class CashRegister
      @items = []
    end
 
-   def add_item(item, price, quantity = 1)
-       @price = price
-       @total += price * quantity
+   def add_item(item,price, quantity = 1)
+     @price = price
+     @total += price*quantity
        if quantity > 1
-         counter = 0
-         while counter < quantity
+         i = 0
+         while i < quantity
            @items << item
-           counter += 1
+           i += 1
          end
-       else
-         @items << item
-       end
+     else
+       @items << item
      end
+   end
 
    def apply_discount
      @total = price * discount
