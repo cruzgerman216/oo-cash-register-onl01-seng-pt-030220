ebe6327c80cb1a68fd8e3517a6eaf2093cdb3103
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 18:58:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
    end
 
    def apply_discount
-     @total = total*@
+     @total = total*0.8
      str =  "After the discount, the total comes to $#{@total.to_i}."
      str
    end
