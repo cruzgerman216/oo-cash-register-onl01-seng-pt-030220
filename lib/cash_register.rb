7f430c37e853f6bcaa2548e9b7d8fe10e577350f
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 18:49:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
      @items = []
    end
 
-   def add_item(title,price, quantity = 0)
-     @total += price
+   def add_item(title,price, quantity = 1)
+     @total += price*quantity
    end
 end
